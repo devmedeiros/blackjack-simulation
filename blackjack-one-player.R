b80fb7dacb96895b9b1c4f9d60937a49c43c4f8c
Jaqueline <medeirosestatistica@gmail.com>
2021-10-21 10:06:35 -0300

    Update blackjack-one-player.R
    Preparing to allow multiple rounds, the function play_one_round() now has a output with the game setup, players that lost or won and total cards remaing in the dealer shoe.

--- blackjack-one-player.R
+++ blackjack-one-player.R
@@ -1,20 +1,19 @@
 #should the dealer hit?
-dealer <- function(dealer_total){
-  if(dealer_total >= 17){
+dealer <- function(dealer_total) {
+  if (dealer_total >= 17) {
     F
   } else T
 }
 
 #should the player hit?
-player <- function(player_total){
-  if(player_total < 21){
-    sample(c(T,F),1)
+player <- function(player_total) {
+  if (player_total < 21) {
+    sample(c(T, F), 1)
   } else F
 }
 
-#a comlpete function to sum the cards
-#there is no case where we have two high aces, but we can have up to 21 low aces
-card_sum <- function(hand){
+#a simple function to sum the cards
+card_sum <- function(hand) {
   
   s_h <- 0 #high ace sum
   s_l <- 0 #low ace sum
@@ -22,19 +21,19 @@ card_sum <- function(hand){
   aces <- sum(hand == "A") #how many aces there is
   a <- 0 #a flag to count if the first ace was counted
   
-  for(i in 1:length(hand)){
-    if(hand[i] %in% c("J", "Q", "K")){
+  for (i in 1:length(hand)) {
+    if (hand[i] %in% c("J", "Q", "K")) {
       s_h <- s_h + 10
       s_l <- s_l + 10
-    } else if(hand[i] == "A"){
-      if(aces == 1){
+    } else if (hand[i] == "A") {
+      if (aces == 1) {
         s_h <- s_h + 11
         s_l <- s_l + 1
-      } else if(a == 0){
+      } else if (a == 0) {
         s_h <- s_h + 11
         s_l <- s_l + 1
         a <- a + 1
-      } else if(a > 0){
+      } else if (a > 0) {
         s_h <- s_h + 1
         s_l <- s_l + 1
       }
@@ -43,104 +42,118 @@ card_sum <- function(hand){
       s_l <- s_l + as.numeric(hand[i])
     }
   }
-  if(s_h > 21){
+  if (s_h > 21) {
     return(s_l)
   } else {
-    return(max(s_h,s_l))
+    return(max(s_h, s_l))
   }
 }
-
-#play one round with as many decks as you wish and as many players
-play_one_round <- function(deck, players){
+#deck_n #of decks
+#deck_c current cards in the deck
+#players #of players
+play_one_round <- function(deck_n, players, deck_c) {
   
-  cards <- rep(c(2:10, "J", "Q", "K", "A"), 4)
-  total_cards <- rep(cards,deck) #total cards being played
+  if (length(deck_c) < 75){
+    #shuffle the deck
+    #aka start with a new deck
+    cards <- rep(c(2:10, "J", "Q", "K", "A"), 4) #cards in a deck
+    total_cards <- rep(cards, deck_n) #total cards in the game
+  } else {
+    #start with the current deck
+    total_cards <- deck_c
+  }
+  
+  #creating the objects
+  cards <- rep(c(2:10, "J", "Q", "K", "A"), 4) #cards in a deck
+  total_cards <- rep(cards, deck) #total cards in the game
   total_cards_played <- 0
+  #shuffle <- F #should the cards be shuffled?
+  players_name <- paste(rep("player", players), 1:players, sep="")
+  players_na <- rep(NA, players) #first value for players
+  players_win_los <- rep(0, players) #1 = player lost
   
-  #each row in the setup is the 'row' round of carts dealt
-  
-  a <- paste(rep("player",players),1:players,sep="")
-  b <- rep(NA,players)
-  c <- rep(F,players)
-  setup_list <- as.list(setNames(c(1,b,NA),c("round",a,"dealer")))
-  sum_list <- as.list(setNames(c(1,b,NA),c("round",a,"dealer")))
-  win_los <- as.list(setNames(c(1,c),c("round",a)))
-  #if the player has lost yet
+  #a list with round, player1, player2, ..., dealer
+  setup_list <- as.list(setNames(c(1, players_na, NA), c("round", players_name, "dealer")))
+  #a list with the sum from players and dealer cards
+  sum_list <- as.list(setNames(c(1, players_na, NA), c("round", players_name, "dealer")))
+  #a list with the information about the win and loss of the players
+  win_los <- as.list(setNames(c(1, players_win_los),c("round", players_name)))
   
   sample_card <- NA
-  n <- players+2
-  m <- players+1
+  n <- players + 2 #to iterate on players and dealer
+  m <- players + 1 #to iterate on players
   
-  #sampling the cards and taking them off the total_cards
-  for(i in 1:2){
-    for(j in 2:n){
+  #dealing the first hand
+  for (i in 1:2) {
+    for (j in 2:n) {
       sample_card <- sample(total_cards, 1)
       total_cards_played <- total_cards_played + 1
       setup_list[[j]][i] <- sample_card
+      #removing the card dealt from the pool of possible cards
       total_cards <- total_cards[-which(total_cards == sample_card)[1]]
     }
   }
   
-  for(u in 2:m){
+  #sum of the player's hand
+  for (u in 2:m) {
     sum_list[[u]] <- card_sum(setup_list[[u]])
   }
   
+  #sum of the dealer's hand (both cards)
   sum_list$dealer <- card_sum(setup_list$dealer)
- 
-  #cat(paste0("The player total is: ", player_total), sep="\n")
-  #cat(paste0("The dealer total is: ", card_sum(setup_list[[n]][1])), sep="\n")
   
-  #player acts
-  for(v in 2:m){
-    k <- 2
+  #how will the players act?
+  for (v in 2:m) {
+    k <- 2 #is 2 because the starting hand is 2 cards
     sample_card <- NA
-    while(player(sum_list[[v]])){
-      cat(paste0("Player Hits."), sep="\n")
+    while (player(sum_list[[v]])) {
       k <- k + 1
       sample_card <- sample(total_cards, 1)
       total_cards_played <- total_cards_played + 1
-      setup_list[[v]][k] <- sample_card
+      setup_list[[v]][k] <- sample_card #adding the new card in the setup_list
       total_cards <- total_cards[-which(total_cards == sample_card)[1]]
-      sum_list[[v]] <- card_sum(setup_list[[v]])
-      cat(paste0(sample_card,"\n" ,"Player new total is: ", sum_list[[v]]), sep="\n")
+      sum_list[[v]] <- card_sum(setup_list[[v]]) #updating the sum
     }
-    if(sum_list[[v]] > 21){
-      cat(paste0("Player ", a[v-1]," Burst and the dealer wins."))
-      win_los[[v]] <- T
-    } else {
-      cat(paste("Player ", a[v-1]," Stands."))
+    #checking if the v player has burst
+    if (sum_list[[v]] > 21) {
+      win_los[[v]] <- 1
     }
   }
   
-    k <- 2
-    sample_card <- NA
+  k <- 2
+  sample_card <- NA
     
-  #dealer acts
-    while(dealer(sum_list$dealer)){
-      total_cards_played <- total_cards_played + 1
-      cat(paste0("Dealer Hits."), sep="\n")
-      k <- k + 1
-      sample_card <- sample(total_cards, 1)
-      setup_list$dealer[k] <- sample_card
-      total_cards <- total_cards[-which(total_cards == sample_card)[1]]
-      sum_list$dealer <- card_sum(setup_list$dealer)
-      cat(paste0(sample_card, "\n","Dealer new total is: ", sum_list$dealer), sep="\n")
-    }
-    
-    cat(paste0("Dealer Stands. With a total of: ", sum_list$dealer), sep="\n")
-    
-    #checking who won
-    if(sum_list$dealer > 21){
-      cat(paste0("The dealer Burst. The remaining players wins."), sep="\n")
-    } else {
-      for(l in 2:m){
-        if(sum_list$dealer >= sum_list[[l]]){
-          cat(paste0("Dealer wins. Against player ", a[l-1]), sep="\n")
-          win_los[[l]] <- T #now this is redundant
-      } else if(sum_list[[l]] > sum_list$dealer){
-          cat(paste0("Player ", a[l-1]," wins."), sep="\n")
-        }
-      }
-    }
-    return(length(total_cards))
+  #dealer acting
+  while (dealer(sum_list$dealer)) {
+    total_cards_played <- total_cards_played + 1
+    k <- k + 1
+    sample_card <- sample(total_cards, 1)
+    setup_list$dealer[k] <- sample_card
+    total_cards <- total_cards[-which(total_cards == sample_card)[1]]
+    sum_list$dealer <- card_sum(setup_list$dealer)
   }
+    
+  if (sum_list$dealer <= 21) {
+    for (l in 2:m) {
+      if (sum_list$dealer >= sum_list[[l]]) {
+        win_los[[l]] <- 1
+      } 
+    }
+  }
+  
+  len <- 0
+  
+  for (t in 1:n) {
+    len[t] <- length(setup_list[[t]])
+  }
+  
+  setup_list$round[2:max(len)] <- 1
+  
+  setup <- as.data.frame(lapply(setup_list, "length<-", max(lengths(setup_list))))
+  output <- list("total_cards" = total_cards, "setup" = setup, "lost" = as.data.frame(win_los))
+  return(output)
+}
+
+play_multiple_rounds <- function(deck, players, rounds){
+  
+}
